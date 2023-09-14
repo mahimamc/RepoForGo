92537bf07532d4810eb03022673d492819f3d6f4
mahimamc <malay.chhinchani@gmail.com>
2023-09-14 15:17:06 +0530

    Todays comitt

---
+++ SpringBoot3DataRestH2/src/main/java/com/app/SpringBoot3DataRestH2Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3DataRestH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3DataRestH2Application.class, args);
+	}
+
+}
